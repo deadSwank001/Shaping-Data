2737f5325ef16e60b84c440ae339658e5bc80acc
deadSwank001 <59746396+deadSwank001@users.noreply.github.com>
2023-06-04 12:47:50 -0500

    Add files via upload

---
+++ Shaping Data.py
@@ -0,0 +1,208 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  4 12:37:40 2023
+
+@author: swank
+"""
+
+# Working with HTML Pages
+
+## Parsing XML and HTML
+
+from lxml import objectify
+import pandas as pd
+from distutils import util
+
+xml = objectify.parse(open('XMLData.xml'))
+root = xml.getroot()
+df = pd.DataFrame(columns=('Number', 'Boolean'))
+
+for i in range(0, 4):
+    obj = root.getchildren()[i].getchildren()
+    row = dict(zip(['Number', 'Boolean'], 
+                   [obj[0].pyval, 
+                    bool(util.strtobool(obj[2].text))]))
+    row_s = pd.Series(row)
+    row_s.name = obj[1].text
+    df = df.append(row_s)
+    
+print(type(df.loc['First']['Number']))
+print(type(df.loc['First']['Boolean']))
+
+## Using XPath for data extraction
+
+from lxml import objectify
+import pandas as pd
+from distutils import util
+
+xml = objectify.parse(open('XMLData.xml'))
+root = xml.getroot()
+
+map_number = map(int, root.xpath('Record/Number'))
+map_bool = map(str, root.xpath('Record/Boolean'))
+map_bool = map(util.strtobool, map_bool)
+map_bool = map(bool, map_bool)
+map_string = map(str, root.xpath('Record/String'))
+
+data = list(zip(map_number, map_bool))
+
+df = pd.DataFrame(data, 
+                  columns=('Number', 'Boolean'), 
+                  index = list(map_string))
+
+print(df)
+print(type(df.loc['First']['Number']))
+print(type(df.loc['First']['Boolean']))
+
+# Working with Raw Text
+
+## Stemming and removing stop words
+
+from sklearn.feature_extraction.text import * 
+from nltk import word_tokenize          
+from nltk.stem.porter import PorterStemmer
+
+stemmer = PorterStemmer()
+
+def stem_tokens(tokens, stemmer):
+    stemmed = []
+    for item in tokens:
+        stemmed.append(stemmer.stem(item))
+    return stemmed
+
+def tokenize(text):
+    tokens = word_tokenize(text)
+    stems = stem_tokens(tokens, stemmer)
+    return stems
+
+vocab = ['Sam loves swimming so he swims all the time']
+vect = CountVectorizer(tokenizer=tokenize, 
+                           stop_words='english')
+vec = vect.fit(vocab)
+
+sentence1 = vec.transform(['George loves swimming too!'])
+
+print(vec.get_feature_names())
+print(sentence1.toarray())
+
+## Introducing regular expressions
+
+import re
+
+data1 = 'My phone number is: 800-555-1212.'
+data2 = '800-555-1234 is my phone number.'
+
+pattern = re.compile(r'(\d{3})-(\d{3})-(\d{4})')
+
+dmatch1 = pattern.search(data1).groups()
+dmatch2 = pattern.search(data2).groups()
+
+print(dmatch1)
+print(dmatch2)
+
+# Using the Bag of Words Model and Beyond
+
+## Understanding the bag of words model
+
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import *
+
+categories = ['comp.graphics', 'misc.forsale', 
+              'rec.autos', 'sci.space']
+twenty_train = fetch_20newsgroups(subset='train',
+                                  categories=categories, 
+                                  shuffle=True, 
+                                  random_state=42)
+
+count_vect = CountVectorizer()
+X_train_counts = count_vect.fit_transform(
+    twenty_train.data)
+
+print("BOW shape:", X_train_counts.shape)
+caltech_idx = count_vect.vocabulary_['caltech']
+print('"Caltech": %i' % X_train_counts[0, caltech_idx]) 
+
+## Working with n-grams
+
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import *
+
+categories = ['sci.space']
+
+twenty_train = fetch_20newsgroups(subset='train', 
+                                  categories=categories, 
+                                  remove=('headers', 
+                                          'footers', 
+                                          'quotes'),
+                                  shuffle=True, 
+                                  random_state=42)
+
+count_chars = CountVectorizer(analyzer='char_wb', 
+                              ngram_range=(3,3), 
+                              max_features=10)
+
+count_chars.fit(twenty_train['data'])
+
+count_words = CountVectorizer(analyzer='word', 
+                              ngram_range=(2,2),
+                              max_features=10,
+                              stop_words='english')
+
+count_words.fit(twenty_train['data'])
+
+X = count_chars.transform(twenty_train.data)
+
+print(count_words.get_feature_names())
+print(X[1].todense())
+print(count_words.get_feature_names())
+
+## Implementing TF-IDF transformations
+
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import *
+
+categories = ['comp.graphics', 'misc.forsale', 
+              'rec.autos', 'sci.space']
+twenty_train = fetch_20newsgroups(subset='train',
+                                  categories=categories, 
+                                  shuffle=True, 
+                                  random_state=42)
+
+count_vect = CountVectorizer()
+X_train_counts = count_vect.fit_transform(
+    twenty_train.data)
+
+tfidf = TfidfTransformer().fit(X_train_counts)
+X_train_tfidf = tfidf.transform(X_train_counts)
+
+caltech_idx = count_vect.vocabulary_['caltech']
+print('"Caltech" scored in a BOW:')
+print('count: %0.3f' % X_train_counts[0, caltech_idx])
+print('TF-IDF: %0.3f' % X_train_tfidf[0, caltech_idx])
+
+import numpy as np
+count = np.mean(X_train_counts[X_train_counts>0])
+tfif = np.mean(X_train_tfidf[X_train_tfidf>0])
+print('mean count: %0.3f' % np.mean(count))
+print('mean TF-IDF: %0.3f' % np.mean(tfif))
+
+# Working with Graph Data
+
+## Using NetworkX basics
+
+import networkx as nx
+
+G = nx.cycle_graph(10)
+A = nx.adjacency_matrix(G)
+
+print(A.todense())
+
+import matplotlib.pyplot as plt
+%matplotlib inline
+nx.draw_networkx(G)
+plt.show()
+
+G.add_edge(1,5)
+nx.draw_networkx(G)
+plt.show()
+
